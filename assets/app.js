6c14567b000dc3b15998441852efcb15a5e6fb74
Alireza <nabikhanialireza738@gmail.com>
2024-03-25 23:52:26 +0330

    add header part&&javascript code to project

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+function back1(){
+    document.getElementById('background-one').style.background = "url('assets/img02.jpg')";
+}
+
+function back2(){
+    document.getElementById('background-one').style.background = "url('assets/img03.jpg')";
+}
+
+function back3(){
+    document.getElementById('background-one').style.background = "url('assets/img04.jpg')";
+}
+
+
+function back4(){
+    document.getElementById('background-one').style.background = "url('assets/img05.jpg')";
+}
